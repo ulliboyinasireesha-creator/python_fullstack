3a0ba44aaee9b73c99863e13dd51fe6949b80bb0
ulliboyinasireesha-creator <ulliboyinasireesha@gmail.com>
2026-07-29 09:58:06 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
 alert("Welcome to NRIIT Learning Management System")
 let heading = document .getElementById
 ("welcome");
-heading.innerHTML = "welcome Future Software Engineers"
+heading.innerHTML = "Welcome Future Software Engineers"
 console.log("Heading element: " , heading)
 let msg= document.getElementById("message")
 msg.innerHTML = "Javascript is fun"
-console.log("Message element: " , msg)
+console.log("Message element: , msg")
 function showmessage () {
     alert("Welcome to NRIIT Learning Management System")
 }
 function changeHeading () {
     document.getElementById("welcome").
     innerHTML = "Welcome Python Fullstack Developers"}
+    let heading1=document.querySelector("#welcome");
+    console.log("Heading element: ", heading1)
+    let button = document.getElementById("btnGreeting");
+    button.addEventListener("click",function(){
+        alert("Welcome to Javascript Event Handling");
+    });
 
 
 
