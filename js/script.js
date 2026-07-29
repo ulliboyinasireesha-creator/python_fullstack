2003679a4ed1f9be36c53ef9fb02d94a5f5c7c53
ulliboyinasireesha-creator <ulliboyinasireesha@gmail.com>
2026-07-29 09:03:50 +0000

    html

--- js/script.js
+++ js/script.js
@@ -3,6 +3,15 @@ let heading = document .getElementById
 ("welcome");
 heading.innerHTML = "welcome Future Software Engineers"
 console.log("Heading element: " , heading)
+let msg= document.getElementById("message")
+msg.innerHTML = "Javascript is fun"
+console.log("Message element: " , msg)
+function showmessage () {
+    alert("Welcome to NRIIT Learning Management System")
+}
+function changeHeading () {
+    document.getElementById("welcome").
+    innerHTML = "Welcome Python Fullstack Developers"}
 
 
 
