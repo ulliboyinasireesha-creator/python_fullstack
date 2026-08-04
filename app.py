98b94279b5d00c842e44cd9ae35876053a66dce9
ulliboyinasireesha-creator <ulliboyinasireesha@gmail.com>
2026-08-04 10:11:43 +0000

    html

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return  render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+@app.route("/courses")
+def courses():
+    return render_template("courses.html")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+@app.route("/login")
+def login():
+    return render_template("login.html")
+@app.route("/register")
+def register():
+    return render_template("register.html")
+@app.route("/trainers")
+def trainers():
+    return render_template("trainers.html")
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/test.js
+++ static/js/test.js
@@ -50,4 +50,3 @@ function checkOddEven(num) {
 }
 console.log("check if 7 is odd or even: ", checkOddEven(7));
 
-
